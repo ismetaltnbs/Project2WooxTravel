161de0af87b14e6d2cf6e10bb95755ad7f926543
ismta <ismta@RIA>
2024-10-08 20:46:31 +0300

    Add project files.

---
+++ Project2WooxTravel/Context/TravelContext.cs
@@ -0,0 +1,17 @@
+﻿using Project2WooxTravel.Entities;
+using System;
+using System.Collections.Generic;
+using System.Data.Entity;
+using System.Linq;
+using System.Web;
+
+namespace Project2WooxTravel.Context
+{
+    public class TravelContext : DbContext 
+     // DbContext -> -Code first yönteminde sql'e yansıyacak tabloların olduğu kısım
+    {
+        public DbSet<Category> Categories { get; set; } //Category entity'nin entities'e dönüşmesini sağlıyoruz.
+        public DbSet<Contact> Contacts { get; set; }
+        public DbSet<Destination> Destinations { get; set; }
+    }
+}
